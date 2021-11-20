c168d2e1d8a427859ffba1632841e515e4cea8f6
Nina Immig <N.M.Immig@student.tudelft.nl>
2021-11-20 12:09:40 +0100

    Improved the Example Page

--- frontend/src/components/examplePage.js
+++ frontend/src/components/examplePage.js
@@ -5,6 +5,8 @@ const text = "This is a short overview of the most common commands in this note 
     "## Those also come in smaller versions \n" +
     "### by adding multiple Hashtags \n" +
     "###### Up to 6 Headline sizes are possible \n" +
-    "You can also format your text using different notions. You can make it **bold**, *italic* or ~strike-through~.";
+    "You can also format your text using different notions. You can make it **bold**, *italic* or ~strike-through~. \n \n" +
+    "You can also create a horizontal line to separate your content: \n \n" +
+    "---------- \n";
 
 export default text;
