ed29c59a729c25666c33cc96bd056e692cd39c64
Nina Immig <N.M.Immig@student.tudelft.nl>
2021-11-20 11:59:31 +0100

    Create and display page on load

--- frontend/src/components/editor.tsx
+++ frontend/src/components/editor.tsx
@@ -1,9 +1,10 @@
 import { h } from "preact";
 import { useState } from "preact/hooks";
 import MDEditor from "@uiw/react-md-editor";
+import text from './examplePage.js';
 
 export default function App() {
-    const [value, setValue] = useState("**Hello world!!!**");
+    const [value, setValue] = useState(text);
     return (
         <div className="container">
             <MDEditor
---
+++ frontend/src/components/examplePage.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+
+const text = "This is a short overview of the most common commands in this note application \n" +
+    "# This is a headline \n" +
+    "## Those also come in smaller versions \n" +
+    "### by adding multiple Hashtags \n" +
+    "###### Up to 6 Headline sizes are possible \n" +
+    "You can also format your text using different notions. You can make it **bold**, *italic* or ~strike-through~.";
+
+export default text;
