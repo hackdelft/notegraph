338ebcb022d15591ecf442e5b5e3e41b745a7f00
Nina Immig <N.M.Immig@student.tudelft.nl>
2021-11-20 13:50:44 +0100

    Added lists to the example

--- frontend/src/components/examplePage.js
+++ frontend/src/components/examplePage.js
@@ -7,6 +7,16 @@ const text = "This is a short overview of the most common commands in this note 
     "###### Up to 6 Headline sizes are possible \n" +
     "You can also format your text using different notions. You can make it **bold**, *italic* or ~strike-through~. \n \n" +
     "You can also create a horizontal line to separate your content: \n \n" +
-    "---------- \n";
+    "---------- \n" +
+    "#### Lists \n" +
+    "* This is an unordered list \n" +
+    "* With multiple items \n  \n" +
+    "1. This is a sorted list \n" +
+    "2. This is the second item in it \n \n" +
+    "You can also create a todo-list: \n" +
+    "- [ ] To-do \n" +
+    "- [x] Done \n" +
+    "- [ ] Also to-do";
+
 
 export default text;
